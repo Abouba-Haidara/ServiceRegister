9595e63ad311b3f818f13b22ef0ddb52c53b5ff9
Yemini Coder <31940004+Abouba-Haidara@users.noreply.github.com>
2020-03-03 14:40:25 +0000

    Initial commit

---
+++ src/main/java/com/info/haidara/serviceregister/ServiceregisterApplication.java
@@ -0,0 +1,14 @@
+package com.info.haidara.serviceregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurakaServer
+@SpringBootApplication
+public class ServiceregisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceregisterApplication.class, args);
+    }
+
+}
